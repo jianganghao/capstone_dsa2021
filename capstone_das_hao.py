1bf23dbef1b0ca8619d02841b50ad62fb83ee499
jianganghao <jiangang1618@gmail.com>
2022-01-06 10:51:39 -0500

    add selection

--- capstone_das_hao.py
+++ capstone_das_hao.py
@@ -13,6 +13,8 @@ df = pd.read_csv('data/data_capstone_dsa2021_2022.csv')
 st.dataframe(df)
 
 st.markdown('## Data Summary')
+select_category = st.selectbox('Please choose the subset',['gender','has_home_computer'])
+st.write(select_category)
 fig_hist = px.histogram(df,x='sum_score')
 st.plotly_chart(fig_hist)
 
