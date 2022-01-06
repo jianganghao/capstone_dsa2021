0a57ed5b741665d79fc518254eac71f2c1240848
jianganghao <jiangang1618@gmail.com>
2022-01-06 11:40:44 -0500

    example text added

--- capstone_das_hao.py
+++ capstone_das_hao.py
@@ -10,7 +10,7 @@ def read_data(file_path_name):
 
 # -- title --
 st.title("Data Safari for Capstone Project")
-st.markdown('This dashboard is for the capstone project of DSA 2021-2022.' )
+st.markdown('This dashboard is an example for the capstone project of DSA 2021-2022.' )
 st.markdown('By: Jiangang Hao')
 st.latex(r'''R_{\mu\nu} - \frac{1}{2}Rg_{\mu\nu} = \frac{8\pi G}{c^4}T_{\mu\nu}''')
 
