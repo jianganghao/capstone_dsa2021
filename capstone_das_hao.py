87f54d56fb167894c6caf707b631015a51163468
jianganghao <jiangang1618@gmail.com>
2022-01-06 10:56:59 -0500

    animation frame added

--- capstone_das_hao.py
+++ capstone_das_hao.py
@@ -15,6 +15,6 @@ st.dataframe(df)
 st.markdown('## Data Summary')
 select_category = st.selectbox('Please choose gender',['Male','Female'])
 st.write(select_category)
-fig_hist = px.histogram(df.query('gender == @select_category'),x='sum_score')
+fig_hist = px.histogram(df.query('gender == @select_category'),x='sum_score',animation_frame = 'home_computer')
 st.plotly_chart(fig_hist)
 
